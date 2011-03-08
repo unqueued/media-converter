558eb02c5eb8f421c3916775e7501ba6107adb99
maartenapple <maartenapple@b363f2ea-6a6c-4f4b-a887-48226118f028>
2011-03-08 12:44:00 +0000

    * You can now tab around the advanced FFmpeg table for advanced editing, real quick ;-)

---
+++ trunk/media-converter/Source/MCOptionsTableView.h
@@ -0,0 +1,17 @@
+//
+//  MCOptionsTableView.h
+//  Media Converter
+//
+//  Created by Maarten Foukhar on 08-03-11.
+//  Copyright 2011 Kiwi Fruitware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MCOptionsTableView : NSTableView
+{
+
+}
+
+@end
