9f0d2607706c406d36a47c1cb9beca8772252267
maartenapple <maartenapple@b363f2ea-6a6c-4f4b-a887-48226118f028>
2011-08-04 09:45:54 +0000

    * Added overlay image creation methods. * Moved batch view tag mapping settings to here so it can be used globally. * Moved some common popup array methods to here.

--- trunk/media-converter/Source/MCCommonMethods.m
+++ trunk/media-converter/Source/MCCommonMethods.m
@@ -8,7 +8,10 @@
 
 #import "MCCommonMethods.h"
 #import "NSString_Extensions.h"
+#import "NSNumber_Extensions.h"
 #import "MCAlert.h"
+#import "MCCheckBoxCell.h"
+#import "MCPopupButton.h"
 
 @interface NSFileManager (MyUndocumentedMethodsForNSTheClass)
 
@@ -420,7 +423,7 @@
 		}
 	
 	if ([[NSUserDefaults standardUserDefaults] boolForKey:@"MCDebug"] == YES) 
-	NSLog(@"%@", commandString);
+		NSLog(@"%@", commandString);
 	
 	return commandString;
 }
@@ -549,6 +552,361 @@
 		return (version >= 5);
 	}
 	#endif
+}
+
++ (NSImage *)overlayImageWithObject:(id)object withSettings:(NSDictionary *)settings inputImage:(NSImage *)image
+{
+	BOOL isString = ([object isKindOfClass:[NSString class]]);
+	BOOL isAttributedString = ([object isKindOfClass:[NSAttributedString class]]);
+
+	NSString *fontName;
+	CGFloat fontSize;
+	NSColor *fontColor;
+	NSString *visibilityMethod;
+	NSColor *borderColor;
+	NSNumber *borderSize;
+	NSColor *boxColor;
+	CGFloat boxMarge = 0;
+	CGFloat boxAlpha;
+
+	if (isString)
+	{
+		fontName = [settings objectForKey:@"Font"];
+		fontSize = [[settings objectForKey:@"Font Size"] cgfloatValue];
+		fontColor = [NSUnarchiver unarchiveObjectWithData:[settings objectForKey:@"Color"]];
+	}
+
+	if (isString | isAttributedString)
+	{
+		visibilityMethod = [settings objectForKey:@"Method"];
+		borderColor = [NSUnarchiver unarchiveObjectWithData:[settings objectForKey:@"Border Color"]];
+		borderSize = [settings objectForKey:@"Border Size"];
+		boxColor = [NSUnarchiver unarchiveObjectWithData:[settings objectForKey:@"Box Color"]];
+		boxMarge = [[settings objectForKey:@"Box Marge"] cgfloatValue];
+		boxAlpha = [[settings objectForKey:@"Box Alpha Value"] cgfloatValue];
+	}
+
+	NSString *hAlignString = [settings objectForKey:@"Horizontal Alignment"];
+	NSString *vAlignString = [settings objectForKey:@"Vertical Alignment"];
+	CGFloat leftMargin = [[settings objectForKey:@"Left Margin"] cgfloatValue];
+	CGFloat rightMargin = [[settings objectForKey:@"Right Margin"] cgfloatValue];
+	CGFloat topMargin = [[settings objectForKey:@"Top Margin"] cgfloatValue];
+	CGFloat bottomMargin = [[settings objectForKey:@"Bottom Margin"] cgfloatValue];
+
+	BOOL border;
+	BOOL box;
+
+	NSMutableAttributedString *attrStr;
+	NSMutableAttributedString *strokeAttrStr;
+
+	if (isString | isAttributedString)
+	{
+		border = ([visibilityMethod isEqualTo:@"border"]);
+		box = ([visibilityMethod isEqualTo:@"box"]);
+
+		if (isAttributedString)
+		{
+			attrStr = [object mutableCopy];
+		}
+		else
+		{
+			NSFont *font = [NSFont fontWithName:fontName size:fontSize];
+		
+			attrStr = [MCCommonMethods initOnMainThreadWithHTML:(NSString *)object];
+
+			NSRange range;
+			NSFontManager *fontManager = [NSFontManager sharedFontManager];
+
+			range = NSMakeRange(0,0);
+		
+			while(NSMaxRange(range) < [attrStr length])
+			{
+				NSDictionary *attributes = [attrStr attributesAtIndex:NSMaxRange(range) effectiveRange:&range];
+				NSFont *oldFont = [attributes objectForKey:NSFontAttributeName];
+				NSFontTraitMask oldTraits = [fontManager traitsOfFont:oldFont];
+				NSFont *newFont = [fontManager convertFont:font toHaveTrait:oldTraits];
+				[attrStr addAttribute:NSFontAttributeName value:newFont range:range];
+			}
+		}
+
+		if (border)
+			strokeAttrStr = [attrStr mutableCopy];
+	}
+	
+	NSImage *subImage = [image copy];
+	NSSize imageSize = [subImage size];
+
+	NSRect objectFrame;
+	
+	if (isString | isAttributedString)
+	{
+		objectFrame = [MCCommonMethods frameForStringDrawing:attrStr forWidth:imageSize.width];
+	}
+	else
+	{
+		NSSize objectSize = [(NSImage *)object size];
+		objectFrame = NSMakeRect(0, 0, objectSize.width, objectSize.height);
+	}
+	
+	CGFloat width;
+	CGFloat height;
+	
+	if (isString | isAttributedString)
+	{
+		width = imageSize.width - leftMargin - rightMargin;
+		height = objectFrame.size.height + 10;
+	}
+	else
+	{
+		width = [[settings objectForKey:@"Width"] intValue];
+		height = [[settings objectForKey:@"Height"] intValue];
+	}
+	
+	CGFloat x = leftMargin;
+	CGFloat y = bottomMargin;
+	
+	if ([vAlignString isEqualTo:@"top"])
+		y = imageSize.height - height - topMargin - (boxMarge * 2);
+	else if ([vAlignString isEqualTo:@"center"])
+		y = (imageSize.height - height) / 2 + (bottomMargin - topMargin);
+	else if ([vAlignString isEqualTo:@"bottom"])
+		y = 0 + bottomMargin;
+	
+	if (isString | isAttributedString)
+	{
+		NSMutableParagraphStyle *centeredStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+	
+		if ([hAlignString isEqualTo:@"center"])
+			[centeredStyle setAlignment:NSCenterTextAlignment];
+		else if ([hAlignString isEqualTo:@"left"])
+			[centeredStyle setAlignment:NSLeftTextAlignment];
+		else if ([hAlignString isEqualTo:@"right"])
+			[centeredStyle setAlignment:NSRightTextAlignment];
+		
+		NSDictionary *attsDict;
+		
+		if (isString)
+			attsDict = [NSDictionary dictionaryWithObjectsAndKeys:centeredStyle, NSParagraphStyleAttributeName, fontColor, NSForegroundColorAttributeName, [NSNumber numberWithInteger:NSNoUnderlineStyle], NSUnderlineStyleAttributeName, nil];
+		else
+			attsDict = attsDict = [NSDictionary dictionaryWithObjectsAndKeys:centeredStyle, NSParagraphStyleAttributeName, nil];
+		
+		[attrStr addAttributes:attsDict range:NSMakeRange(0, [[attrStr string] length])];
+		
+		NSDictionary *strokeAttsDict;
+		
+		if (border)
+			strokeAttsDict = [NSDictionary dictionaryWithObjectsAndKeys:borderSize, NSStrokeWidthAttributeName , borderColor, NSStrokeColorAttributeName, centeredStyle, NSParagraphStyleAttributeName, [NSNumber numberWithInteger:NSNoUnderlineStyle], NSUnderlineStyleAttributeName, nil];
+		
+		[centeredStyle release];
+		
+		if (border)
+			[strokeAttrStr addAttributes:strokeAttsDict range:NSMakeRange(0, [[strokeAttrStr string] length])];
+	}
+	else
+	{
+		if ([hAlignString isEqualTo:@"center"])
+			x = leftMargin + ((imageSize.width - width) / 2) - rightMargin;
+		else if ([hAlignString isEqualTo:@"right"])
+			x = (imageSize.width - width - rightMargin);
+	}
+		
+	[subImage lockFocus];
+	
+	if (isString | isAttributedString)
+	{
+		if (box)
+		{
+			NSSize attrSize = [attrStr size];
+			NSInteger boxX;
+			
+			if ([hAlignString isEqualTo:@"center"])
+				boxX = leftMargin + ((imageSize.width - attrSize.width) / 2) - rightMargin - boxMarge;
+			else if ([hAlignString isEqualTo:@"right"])
+				boxX = (imageSize.width - attrSize.width - rightMargin - boxMarge);
+			else
+				boxX = x;
+		
+			[[boxColor colorWithAlphaComponent:boxAlpha] set];
+			
+			NSBezierPath *path = [NSBezierPath bezierPathWithRect:NSMakeRect(boxX, y, attrSize.width + boxMarge * 2, height + boxMarge * 2)];
+			[path fill];
+		}
+		
+		if (!box)
+			boxMarge = 0;
+	
+		[attrStr drawInRect:NSMakeRect(x + boxMarge, y + boxMarge, width - (boxMarge * 2), height)];
+		
+		if (border)
+			[strokeAttrStr drawInRect:NSMakeRect(x, y, width, height)];
+
+		[attrStr release];
+		attrStr = nil;
+		
+		if (border)
+		{
+			[strokeAttrStr release];
+			strokeAttrStr = nil;
+		}
+	}
+	else
+	{
+		[(NSImage *)object drawInRect:NSMakeRect(x, y, width, height) fromRect:NSZeroRect operation:NSCompositeCopy fraction:1.0];
+	}
+		
+	[subImage unlockFocus];
+		
+	return [subImage autorelease];
+}
+
++ (NSMutableAttributedString *)initOnMainThreadWithHTML:(NSString *)html
+{
+	SEL theSelector = @selector(initWithHTML:);
+	NSMethodSignature *aSignature;
+	NSInvocation *anInvocation;
+
+	//Get the methods signature and set the selector
+	aSignature = [[MCCommonMethods class] instanceMethodSignatureForSelector:theSelector];
+	anInvocation = [NSInvocation invocationWithMethodSignature:aSignature];
+	[anInvocation setSelector:theSelector];
+	//Set arguments
+	[anInvocation setArgument:&html atIndex:2];
+	//Perform selector
+	MCCommonMethods *object = [[MCCommonMethods alloc] init];
+	
+	[anInvocation performSelectorOnMainThread:@selector(invokeWithTarget:) withObject:object waitUntilDone:YES];
+	
+	NSMutableAttributedString *attString;
+	[anInvocation getReturnValue:&attString];
+	
+	return attString;
+}
+
+- (NSMutableAttributedString *)initWithHTML:(NSString *)html
+{
+	return [[NSMutableAttributedString alloc] initWithHTML:[html dataUsingEncoding:NSUTF8StringEncoding] options:[NSDictionary dictionaryWithObjectsAndKeys:@"utf-8", @"TextEncodingName", nil] documentAttributes:nil];
+}
+
++ (NSRect)frameForStringDrawing:(NSAttributedString *)myString forWidth:(float)myWidth
+{
+	NSTextStorage *textStorage = [[[NSTextStorage alloc] initWithAttributedString:myString] autorelease];
+	NSTextContainer *textContainer = [[[NSTextContainer alloc] initWithContainerSize:NSMakeSize(myWidth, FLT_MAX)] autorelease];;
+	NSLayoutManager *layoutManager = [[[NSLayoutManager alloc] init] autorelease];
+	[layoutManager addTextContainer:textContainer];
+	[textStorage addLayoutManager:layoutManager];
+	[textContainer setLineFragmentPadding:0.0];
+
+	(void) [layoutManager glyphRangeForTextContainer:textContainer];
+	return [layoutManager usedRectForTextContainer:textContainer];
+}
+
++ (void)setViewOptions:(NSArray *)views infoObject:(id)info fallbackInfo:(id)fallback mappingsObject:(NSArray *)mappings startCount:(NSInteger)start
+{
+	NSEnumerator *iter = [[[NSEnumerator alloc] init] autorelease];
+	NSControl *cntl;
+
+	NSInteger x;
+	for (x = 0; x < [views count]; x ++)
+	{
+		NSView *currentView;
+		
+		if ([[views objectAtIndex:x] isKindOfClass:[NSView class]])
+			currentView = [views objectAtIndex:x];
+		else
+			currentView = [[views objectAtIndex:x] view];
+		
+		iter = [[currentView subviews] objectEnumerator];
+		while ((cntl = [iter nextObject]) != NULL)
+		{
+			NSInteger tag = [cntl tag] - start;
+
+			if ([cntl isKindOfClass:[NSTabView class]])
+			{
+				[MCCommonMethods setViewOptions:[(NSTabView *)cntl tabViewItems] infoObject:info fallbackInfo:fallback mappingsObject:mappings startCount:start];
+			}
+			else if ([cntl isKindOfClass:[NSBox class]])
+			{
+				[MCCommonMethods setViewOptions:[(NSBox *)cntl subviews] infoObject:info fallbackInfo:fallback mappingsObject:mappings startCount:start];
+			}
+			else if (tag > 0)
+			{
+				NSInteger index = tag - 1;
+
+				if (index < [mappings count])
+				{
+					NSString *currentKey = [mappings objectAtIndex:index];
+					id property = [info objectForKey:currentKey];
+					
+					if (property == nil && fallback != nil)
+						property = [fallback objectForKey:currentKey];
+					
+					[MCCommonMethods setProperty:property forControl:cntl];
+
+					property = nil;
+				}
+			}
+		}
+	}
+}
+
++ (void)setProperty:(id)property forControl:(id)control
+{
+	if (property)
+	{
+		if ([control isKindOfClass:[NSTextField class]])
+		{
+			[control setObjectValue:property];
+		}
+		else if ([[control cell] isKindOfClass:[MCCheckBoxCell class]])
+		{
+			[(MCCheckBoxCell *)[control cell] setStateWithoutSelecting:NSOnState];
+		}
+		else
+		{
+			[control setObjectValue:property];
+		}
+						
+		[control setEnabled:YES];
+	}
+	else if ([control isKindOfClass:[MCPopupButton class]])
+	{
+		[(MCPopupButton *)control selectItemAtIndex:0];
+	}
+	else if (![control isKindOfClass:[NSButton class]])
+	{
+		if ([control tag] < 100)
+			[control setEnabled:NO];
+		else
+			[control setStringValue:@""];
+	}
+	else if ([[control cell] isKindOfClass:[MCCheckBoxCell class]])
+	{
+		[(MCCheckBoxCell *)[control cell] setStateWithoutSelecting:NSOffState];
+	}
+	else if ([control isKindOfClass:[NSButton class]])
+	{
+		[control setState:NSOffState];
+	}
+}
+
++ (NSArray *)defaultHorizontalPopupArray
+{
+	NSMutableArray *horizontalAlignments = [NSMutableArray array];
+	[horizontalAlignments insertObject:[NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Left", nil), @"Name", @"left", @"Format", nil] atIndex:0];
+	[horizontalAlignments insertObject:[NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Center", nil), @"Name", @"center", @"Format", nil] atIndex:1];
+	[horizontalAlignments insertObject:[NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Right", nil), @"Name", @"right", @"Format", nil] atIndex:2];
+	
+	return horizontalAlignments;
+}
+
++ (NSArray *)defaultVerticalPopupArray
+{
+	NSMutableArray *verticalAlignments = [NSMutableArray array];
+	[verticalAlignments insertObject:[NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Top", nil), @"Name", @"top", @"Format", nil] atIndex:0];
+	[verticalAlignments insertObject:[NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Center", nil), @"Name", @"center", @"Format", nil] atIndex:1];
+	[verticalAlignments insertObject:[NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Bottom", nil), @"Name", @"bottom", @"Format", nil] atIndex:2];
+	
+	return verticalAlignments;
 }
 
 @end
