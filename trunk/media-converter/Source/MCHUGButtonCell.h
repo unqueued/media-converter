9972dc7074511c10fe97dd34abae11452e21f1d4
maartenapple <maartenapple@b363f2ea-6a6c-4f4b-a887-48226118f028>
2011-08-05 14:12:55 +0000

    * Added HUG button class for preview window button.

---
+++ trunk/media-converter/Source/MCHUGButtonCell.h
@@ -0,0 +1,19 @@
+//
+//  MCHUGButtonCell.h
+//  Media Converter
+//
+//  Created by Maarten Foukhar on 05-08-11.
+//  Copyright 2011 Kiwi Fruitware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MCHUGButtonCell : NSButtonCell
+{
+
+}
+
+- (NSImage *)buttonImageInFrame:(NSRect)frame;
+
+@end
