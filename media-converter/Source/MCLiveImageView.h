e7acb8ec695979759948fa8fa033d5c8c1db881f
maartenapple <maarten@mac-mini.lan>
2011-09-05 06:16:52 +0000

    * Created dynamic subtitle / filter preview creation (makes it easier to test for different resolutions).

---
+++ media-converter/Source/MCLiveImageView.h
@@ -0,0 +1,17 @@
+//
+//  MCLiveImageView.h
+//  Media Converter
+//
+//  Created by Maarten Foukhar on 04-09-11.
+//  Copyright 2011 Kiwi Fruitware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MCLiveImageView : NSImageView
+{
+
+}
+
+@end
